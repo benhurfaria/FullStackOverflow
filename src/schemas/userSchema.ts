fc7543cf8461c1317d4fe00d41d0ab3d0c542296
Matheus Galdino <matheus@respondeai.com.br>
2021-12-14 17:36:58 -0300

    preparando o review

--- src/schemas/userSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const userSchema: joi.ObjectSchema = joi.object({
-  name: joi.string().min(3).required(),
-  class: joi.string().max(3).required(),
-});
-
-export { userSchema };
